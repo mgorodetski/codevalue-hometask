af9e77d362d135a5963e81a3add0a12fab1d80cc
mgorodetski <87032361+mgorodetski@users.noreply.github.com>
2022-11-21 21:15:50 +0200

    sortBy refactored to literals

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC } from 'react'
+import React, { ChangeEvent, FC } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addProductAction, sortByDateAction, sortByNameAction } from '../../actions/productActions';
@@ -21,15 +21,26 @@ const SearchBar: FC<Props> = ({ setFilter }) => {
         }));
     }
 
-    const sortBy = (e: ChangeEvent<HTMLSelectElement>) => {
-        switch (e.target.value) {
-            case 'name':
-                dispatch(sortByNameAction());
-                break;
-            case 'dateCreated':
-                dispatch(sortByDateAction());
-                break;
-        }       
+
+
+    // const sortBy = (e: ChangeEvent<HTMLSelectElement>) => {
+    //     switch (e.target.value) {
+    //         case 'name':
+    //             dispatch(sortByNameAction());
+    //             break;
+    //         case 'dateCreated':
+    //             dispatch(sortByDateAction());
+    //             break;
+    //     }
+    // }
+
+    const sortActionType = {
+        name: sortByNameAction(),
+        dateCreated: sortByDateAction()
+    }
+    const sortByWithLiteral = (e: ChangeEvent<HTMLSelectElement>) => {
+        const getSortAction = (value: string) => dispatch((sortActionType as any)[value]);
+        getSortAction(e.target.value);
     }
 
     const findProductsByName = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,7 +52,7 @@ const SearchBar: FC<Props> = ({ setFilter }) => {
             <button type="button" onClick={addDummyProduct} className="btn">+ Add</button>
             <input type="search" onChange={findProductsByName} placeholder="Search product" />
             <span> Sort by: </span>
-            <select id="sort" onChange={sortBy}>
+            <select id="sort" onChange={sortByWithLiteral}>
                 <option value="name">Name</option>
                 <option value="dateCreated">Recently added</option>
             </select>
